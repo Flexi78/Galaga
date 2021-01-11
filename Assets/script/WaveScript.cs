c96d3979d05be527a06112776b19935b982990dd
Flex <felix.litou@gmail.com>
2021-01-11 20:02:12 +0100

    Création de vagues

---
+++ Assets/script/WaveScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveScript : MonoBehaviour
+{
+    public GameObject[] AlienType;
+    public float SpaceColums = 1f, SpaceRow = 1f;
+    public int TotalAliensInline = 3;
+    public Transform SpawnPoint;
+
+    private void Awake()
+    {
+        //Générteur de vague
+
+        for (int i=0; i<AlienType.Length; i++)
+        {
+            float posY = transform.position.y - (SpaceRow * i);
+
+            for (int j=0; j<TotalAliensInline; j++)
+            {
+                Vector2 position = new Vector2(this.transform.position.x + (SpaceColums * j), posY);
+                GameObject Go = Instantiate(AlienType[i].gameObject, position, Quaternion.identity);
+                Go.transform.SetParent(this.transform);
+            }
+        }
+
+    }
+}
