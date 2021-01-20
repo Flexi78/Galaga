4cb53b61c3cd5320c4373563c069d98daace9036
Flex <felix.litou@gmail.com>
2021-01-20 16:30:17 +0100

    commit 20/01

--- Assets/script/WaveScript.cs
+++ Assets/script/WaveScript.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class WaveScript : MonoBehaviour
 {
-    public GameObject[] AlienType;
+    public GameObject[] NbRaws;
     public float SpaceColums = 1f, SpaceRow = 1f;
     public int TotalAliensInline = 3;
     public Transform SpawnPoint;
 
+
+
     private void Awake()
     {
-        //Générteur de vague
+        //Générteur de vague //TEST
 
-        for (int i=0; i<AlienType.Length; i++)
+        for (int i=0; i<NbRaws.Length; i++)
         {
-            float posY = transform.position.y - (SpaceRow * i);
+            float posY = this.transform.position.y - (SpaceRow * i);
 
             for (int j=0; j<TotalAliensInline; j++)
             {
-                Vector2 position = new Vector2(this.transform.position.x + (SpaceColums * j), posY);
-                GameObject Go = Instantiate(AlienType[i].gameObject, position, Quaternion.identity);
+                Vector3 position = new Vector3(this.transform.position.x + (SpaceColums * j), posY, SpawnPoint.transform.position.z);
+                GameObject Go = Instantiate(NbRaws[i].gameObject, position, Quaternion.identity);
                 Go.transform.SetParent(this.transform);
             }
         }
 
     }
+
 }
--- Assets/script/WaypointScript.cs
+++ Assets/script/WaypointScript.cs
@@ -11,6 +11,7 @@ public class WaypointScript : MonoBehaviour
     public GameObject[] Waypoint;
     public GameObject ObjectToMove;
     public float speed = 5;
+    public int subDivCount = 25;
     [SerializeField]
     private int i = 0;
 
@@ -27,10 +28,25 @@ public class WaypointScript : MonoBehaviour
 
     private void Update()
     {
-        if (i < transform.childCount)
+       if (i < transform.childCount)
         {
             if (ObjectToMove.transform.position != Waypoint[i].transform.position)
             {
+                /*for (int j=1; j < Waypoint.Length - 3; i += 2)
+                {
+                    Vector3 a, b, c, dest;
+                    a = Waypoint[j].transform.position;
+                    b = Waypoint[j+1].transform.position;
+                    c = Waypoint[j+2].transform.position;
+
+                    for (int n =0; n < subDivCount; n++)
+                    {
+                        dest = BezierPath(a, b, c, n / (float)subDivCount);
+                        ObjectToMove.transform.position = Vector3.MoveTowards(ObjectToMove.transform.position, dest, speed * Time.deltaTime);
+
+                    }
+                }*/
+
                 ObjectToMove.transform.position = Vector3.MoveTowards(ObjectToMove.transform.position, Waypoint[i].transform.position, speed * Time.deltaTime);
             }
             else
@@ -39,6 +55,22 @@ public class WaypointScript : MonoBehaviour
             }
         }
     }
+
+    Vector3 BezierPath(Vector3 p0, Vector3 p1, Vector3 p2, float progress)
+    {
+        Vector3 controlNodeA = LinearInterp(p0, p1, progress);
+        Vector3 controlNodeB = LinearInterp(p1, p2, progress);
+
+        return LinearInterp(controlNodeA, controlNodeB, progress);
+    }
+
+    Vector3 LinearInterp(Vector3 start, Vector3 end, float progress)
+    {
+        return new Vector3(LinearInterp(start.x, end.x, progress), LinearInterp(start.y, end.y, progress), LinearInterp(start.z, end.z, progress));
+    }
+
+    float LinearInterp(float start, float end, float progress)
+    {
+        return start + (end - start) * progress;
+    }
 }
-
-
