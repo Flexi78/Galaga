5732dea7eaf00843cf8f312a229b2201d37dd448
Flex <felix.litou@gmail.com>
2021-01-20 16:37:41 +0100

    fix ennemi

--- Assets/script/Ennemi.cs
+++ Assets/script/Ennemi.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*public class Ennemi : MonoBehaviour
+public class Ennemi : MonoBehaviour
 {    
     [Header("Ennemi settings")]
     [SerializeField] Transform m_SpawnPoint;
@@ -30,84 +30,14 @@ using UnityEngine;
         
     }
 
-    /*private void OnTriggerEnter(Collider collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Ball"))
         {
             Destroy(this);
         }
         
-    }*/
-//}
-
-public class Ennemi : MonoBehaviour
-{
-    [SerializeField]
-    public int enemyRows = 3;
-    public int enemyColumns = 10;
-    public int direction = 1;
-    public float speed = 1;
-    public GameObject enemyPref;
-    public GameObject[] enemies;
-    private GameObject attackForce;
-
-    int GetEnemyCount()
-    {
-        int total = 0;
-        foreach (GameObject enemy in enemies)
-        {
-            if (enemy != null)
-            {
-                total += 1;
-            }
-        }
-        return total;
     }
-
-    void Start()
-    {
-        enemies = new GameObject[enemyRows * enemyColumns];
-
-        Spawn();
-    }
-
-    void Spawn()
-    {
-        Vector2 center = gameObject.transform.position;
-        for (int i = 0; i < enemyRows; i++)
-        {
-            for (int j = 0; j < enemyColumns; j++)
-            {
-                if (!enemies[i * enemyColumns + j])
-                {
-                    Vector2 pos = new Vector2(j - enemyColumns / 2 + 0.5f, i);
-                    enemies[i * enemyColumns + j] = (GameObject)Instantiate(enemyPref, pos, Quaternion.identity);
-                    enemies[i * enemyColumns + j].transform.SetParent(this.transform);
-                }
-            }
-        }
-    }
-
-    void Move()
-    {
-        var pos = transform.position;
-        pos.x = pos.x + direction * Time.deltaTime * speed;
-        pos.x = Mathf.Clamp(pos.x, -1, 1);
-
-        transform.position = pos;
-
-        if (pos.x == -1 || pos.x == 1)
-        {
-            direction = -1 * direction;
-        }
-    }
-
-    bool isAttacking()
-    {
-        return (bool)attackForce;
-    }
-
 }
-
 
 
